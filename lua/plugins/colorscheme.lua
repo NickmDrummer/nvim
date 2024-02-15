648180bff99aada595f754cea66ddf024236592f
Nick <nickmdrummer@gmail.com>
2024-02-14 23:38:10 -0300

    Adding files

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,14 @@
+return {
+  "folke/tokyonight.nvim",
+  opts = {
+    transparent = false,
+    styles = {
+      -- sidebars = "transparent",
+      -- floats = "transparent",
+      comments = "italic",
+      keywords = "italic",
+      functions = "italic",
+    },
+  },
+}
+-- este es un ejemplo de un comentario
---
+++ lua/plugins/solarized-osaka.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "craftzdog/solarized-osaka.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+        comments = "italic",
+        keywords = "italic",
+        functions = "italic",
+      },
+    },
+  },
+}
