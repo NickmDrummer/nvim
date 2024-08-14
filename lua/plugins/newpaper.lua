a37c56c34d6fb9e58cdb501a64b9fc7369246835
NickmDrummer <nickmdrummer@gmail.com>
2024-08-14 02:02:49 -0300

    newpaper.nvim y haskell support

---
+++ lua/plugins/newpaper.lua
@@ -0,0 +1,10 @@
+return {
+  "yorik1984/newpaper.nvim",
+  priority = 1000,
+  lazy = false,
+  config = true,
+  opts = {
+    saturation = 0.1,
+    terminal = "inverse_transparent", -- default: "contrast"
+  },
+}
