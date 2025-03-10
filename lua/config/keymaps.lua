589b19dfc4c438cee96e991e52b4e80affa2e4e0
NickmDrummer <nickmdrummer@gmail.com>
2025-03-10 00:07:26 -0300

    new: keymap to formatting

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -21,3 +21,7 @@ vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><
 
 -- Add a custom keybinding to toggle the colorscheme
 vim.api.nvim_set_keymap("n", "<leader>tt", ":CyberdreamToggleMode<CR>", { noremap = true, silent = true })
+
+vim.keymap.set("n", "<leader>cf", function()
+  vim.lsp.buf.format({ async = false })
+end, { desc = "Formatting" })
