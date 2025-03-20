74de7ef9c61fbbf37ebe0e32fd124d54096dfecf
NickmDrummer <nickmdrummer@gmail.com>
2025-03-20 02:47:16 -0300

    mod: redesigned lualile

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,62 +1,84 @@
---
--- lualine.lua
---
--- Custom status line
---
-
 return {
   "nvim-lualine/lualine.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
-  event = "VeryLazy",
+
   config = function()
-    -- Custom Lualine component to show attached language server
-    local clients_lsp = function()
-      local clients = vim.lsp.get_clients()
-      if next(clients) == nil then
-        return ""
-      end
+    local actualTheme = require("lualine.themes.cyberdream")
+    local cyberdream = {
+      black = "#000000",
+      black2 = "#000002",
+      white = "#ffffff",
+      red = "#ff6e5e",
+      green = "#5eff6c",
+      blue = "#04d1f9",
+      yellow = "#f1ff5e",
+      cyan = "#5ef1ff",
+      orange = "#ffbd5e",
+      pureOrange = "#ff5e00",
+      magenta = "#ff5ef1",
+      pink = "#ff5ea0",
+      purple = "#bd5eff",
+    }
+    -- section Mode
+    actualTheme.normal.a.bg = cyberdream.blue
+    actualTheme.normal.a.fg = cyberdream.black2
+    actualTheme.insert.a.bg = cyberdream.green
+    actualTheme.insert.a.fg = cyberdream.black2
+    actualTheme.visual.a.bg = cyberdream.purple
+    actualTheme.visual.a.fg = cyberdream.white
+    actualTheme.terminal.a.bg = cyberdream.pureOrange
+    actualTheme.terminal.a.fg = cyberdream.white
 
-      local c = {}
-      for _, client in pairs(clients) do
-        table.insert(c, client.name)
-      end
-      return " " .. table.concat(c, "|")
-    end
+    -- section Branch
+    actualTheme.normal.b.fg = cyberdream.white
 
-    -- NOTE: This is the theme I use
-    -- BUG: the theme option is not working and I have to force the colors
-    -- local theme = require("lualine.themes.rose-pine")
+    -- section Progress
+    actualTheme.normal.y.bg = cyberdream.black2
+    actualTheme.normal.y.fg = cyberdream.pink
+
+    -- section Location
+    actualTheme.normal.z.bg = cyberdream.pink
+    actualTheme.normal.z.fg = cyberdream.black2
+    actualTheme.insert.z.bg = cyberdream.pink
+    actualTheme.insert.z.fg = cyberdream.black2
+    actualTheme.visual.z.bg = cyberdream.pink
+    actualTheme.visual.z.fg = cyberdream.black2
+    actualTheme.terminal.z.bg = cyberdream.pink
+    actualTheme.terminal.z.fg = cyberdream.black2
 
     require("lualine").setup({
       options = {
-        theme = "auto",
-        component_separators = "",
+        icons_enabled = true,
+        theme = actualTheme,
+        component_separators = { left = "⏽", right = "⏽" },
         section_separators = { left = "", right = "" },
-        disabled_filetypes = { "alpha", "Outline" },
+        disabled_filetypes = {
+          statusline = {},
+          winbar = {},
+        },
+        ignore_focus = {},
+        always_divide_middle = true,
+        always_show_tabline = true,
+        globalstatus = false,
+        refresh = {
+          statusline = 100,
+          tabline = 100,
+          winbar = 100,
+        },
       },
       sections = {
         lualine_a = {
-          { "mode", separator = { left = "", right = "" }, icon = "" },
+          "mode",
         },
         lualine_b = {
-          {
-            "filetype",
-            icon_only = true,
-            padding = { left = 1, right = 0 },
-          },
-          "filename",
-        },
-        lualine_c = {
-          {
-            "branch",
-            icon = "",
-          },
+          { "branch", icon = " " },
           {
             "diff",
             symbols = { added = " ", modified = " ", removed = " " },
             colored = true,
           },
         },
+        lualine_c = {},
         lualine_x = {
           {
             "diagnostics",
@@ -64,21 +86,26 @@ return {
             update_in_insert = true,
             colored = true,
           },
+          { "filetype", icon_only = true },
+          "filename",
         },
-        lualine_y = { clients_lsp },
-        lualine_z = {
-          { "location", separator = { left = "", right = "" }, icon = "" },
+        lualine_y = {
+          "progress",
         },
+        lualine_z = { { "location", icon = " " } },
       },
       inactive_sections = {
-        lualine_a = { "filename" },
+        lualine_a = {},
         lualine_b = {},
-        lualine_c = {},
-        lualine_x = {},
+        lualine_c = { "filename" },
+        lualine_x = { "location" },
         lualine_y = {},
-        lualine_z = { "location" },
+        lualine_z = {},
       },
-      extensions = { "toggleterm", "trouble" },
+      tabline = {},
+      winbar = {},
+      inactive_winbar = {},
+      extensions = {},
     })
   end,
 }
