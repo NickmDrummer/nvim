62a73e9abf7c4d01ac67a8f67922518b3256750d
NickmDrummer <nickmdrummer@gmail.com>
2025-03-31 16:43:46 -0300

    fix: reestructuring confom config

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,30 +1,29 @@
 return {
-  {
     "stevearc/conform.nvim",
-    -- changing conform.nvim
-    opts = function()
-      local opts = {
+    event = { "BufWritePre" },
+    opts = {
         formatters_by_ft = {
-          go = { "gofumpt", "goimports" },
-          c = { "clang-format" },
-          cs = { "csharpier" },
-          html = { "prettier" },
-          javascript = { "prettier" },
-          javascriptreact = { "prettier" },
-          json = { "prettier" },
-          typescript = { "prettier" },
-          typescriptreact = { "prettier" },
-          yaml = { "prettier" },
-          lua = { "stylua" },
+            go = { "gofumpt", "goimports" },
+            c = { "clang-format" },
+            cpp = { "clang-format" },
+            cs = { "csharpier" },
+            haskell = { "ormolu" },
+            html = { "prettier" },
+            markdown = { "prettier" },
+            sh = { "shfmt" },
+            javascript = { "prettier" },
+            javascriptreact = { "prettier" },
+            json = { "prettier" },
+            typescript = { "prettier" },
+            typescriptreact = { "prettier" },
+            yaml = { "prettier" },
+            lua = { "stylua" },
         },
         formatter = {
-          csharpier = {
-            command = "dotnet-csharpier",
-            args = { "--write-stdout" },
-          },
+            csharpier = {
+                command = "dotnet-csharpier",
+                args = { "--write-stdout" },
+            },
         },
-      }
-      return opts
-    end,
-  },
+    },
 }
