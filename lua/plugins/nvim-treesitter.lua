297eb48231abf6344dbc9349e40401001569b985
NickmDrummer <nickmdrummer@gmail.com>
2025-04-22 00:42:28 -0300

    fix: deleting haskell, adding templ from treesitter opts

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,7 +1,7 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    opts = { ensure_installed = { "c", "cpp", "lua", "go", "markdown", "haskell", "json" } },
+    opts = { ensure_installed = { "c", "cpp", "lua", "go", "markdown", "json", "templ" } },
     indent = {
       enable = true,
       -- disable = { "yaml" }
