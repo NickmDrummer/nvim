f3f0e72be2b5f2e2a31b06a6ab0c44d4d9388500
NickmDrummer <nickmdrummer@gmail.com>
2025-04-22 00:41:29 -0300

    new: templ config

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,6 +1,13 @@
 return {
   "neovim/nvim-lspconfig",
   opts = {
+    servers = {
+      templ = {
+        cmd = { "templ", "lsp" },
+        filetypes = { "templ" },
+        root_dir = require("lspconfig.util").root_pattern("go.mod", ".git"),
+      },
+    },
     setup = {
       clangd = function(_, opts)
         opts.capabilities.offsetEncoding = { "utf-16" }
