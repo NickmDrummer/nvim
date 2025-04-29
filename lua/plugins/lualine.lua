e24689fd9f7d88684e54876fa08ce5bf276da841
NickM <nickmdrummer@gmail.com>
2025-04-29 19:16:50 -0300

    change lualine style without bg colors

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -21,38 +21,38 @@ return {
 
     local function apply_custom_colors(theme)
       -- section Mode
-      theme.normal.a.bg = cyberdream.blue
-      theme.normal.a.fg = cyberdream.black2
+      theme.normal.a.bg = cyberdream.black
+      theme.normal.a.fg = cyberdream.blue
       theme.normal.a.gui = "bold"
-      theme.insert.a.bg = cyberdream.green
-      theme.insert.a.fg = cyberdream.black2
+      theme.insert.a.bg = cyberdream.black
+      theme.insert.a.fg = cyberdream.green
       theme.insert.a.gui = "bold"
-      theme.visual.a.bg = cyberdream.purple
-      theme.visual.a.fg = cyberdream.white
+      theme.visual.a.bg = cyberdream.black
+      theme.visual.a.fg = cyberdream.purple
       theme.visual.a.gui = "bold"
-      theme.terminal.a.bg = cyberdream.pureOrange
-      theme.terminal.a.fg = cyberdream.white
+      theme.terminal.a.bg = cyberdream.black
+      theme.terminal.a.fg = cyberdream.pureOrange
       theme.terminal.a.gui = "bold"
 
       -- section Branch
       theme.normal.b.fg = cyberdream.white
 
       -- section Progress
-      theme.normal.y.bg = cyberdream.black2
+      theme.normal.y.bg = cyberdream.black
       theme.normal.y.fg = cyberdream.red
 
       -- section Location
-      theme.normal.z.bg = cyberdream.green
-      theme.normal.z.fg = cyberdream.black2
+      theme.normal.z.bg = cyberdream.black
+      theme.normal.z.fg = cyberdream.green
       theme.normal.z.gui = "bold"
-      theme.insert.z.bg = cyberdream.green
-      theme.insert.z.fg = cyberdream.black2
+      theme.insert.z.bg = cyberdream.black
+      theme.insert.z.fg = cyberdream.green
       theme.insert.z.gui = "bold"
-      theme.visual.z.bg = cyberdream.green
-      theme.visual.z.fg = cyberdream.black2
+      theme.visual.z.bg = cyberdream.black
+      theme.visual.z.fg = cyberdream.green
       theme.visual.z.gui = "bold"
-      theme.terminal.z.bg = cyberdream.green
-      theme.terminal.z.fg = cyberdream.black2
+      theme.terminal.z.bg = cyberdream.black
+      theme.terminal.z.fg = cyberdream.green
       theme.terminal.z.gui = "bold"
       return theme
     end
