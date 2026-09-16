069ec9c17acd041186038a8c6bc7e77c87c52075
Nick Marasciuolo <nickmdrummer@gmail.com>
2026-09-16 20:31:05 -0300

    (mfd-theme): Sync cursor highlights to the terminal

--- lua/plugins/mfd_theme.lua
+++ lua/plugins/mfd_theme.lua
@@ -4,5 +4,12 @@ return {
   priority = 1000,
   config = function()
     vim.cmd("colorscheme mfd-nerv")
+    vim.opt.guicursor = {
+      "n:block-CursorNormal",
+      "v:block-CursorVisual",
+      "i:block-CursorInsert",
+      "r-cr:block-CursorReplace",
+      "c:block-CursorCommand",
+    }
   end,
 }
