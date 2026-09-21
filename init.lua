3f0f600e841026497b49a489362261ea474c123d
Nick Marasciuolo <nickmdrummer@gmail.com>
2026-09-21 13:43:28 -0300

    (init.lua): fix keep normal yanks in the wayland clipboard

--- init.lua
+++ init.lua
@@ -1,5 +1,7 @@
 -- forcing block cursor
 vim.opt.guicursor = "n-v-c-i:block"
+vim.opt.clipboard = "unnamedplus" -- Keep normal yanks in the Wayland clipboard
 vim.o.timeoutlen = 2000
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+vim.opt.clipboard = "unnamedplus" -- Final setting after LazyVim startup
