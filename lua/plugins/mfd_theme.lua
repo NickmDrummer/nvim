66db54c254071d6bb085e0c13e5e9baf5bae679f
Nick Marasciuolo <nickmdrummer@gmail.com>
2026-09-24 14:40:15 -0300

    (mfd-theme): disable sync cursor

--- lua/plugins/mfd_theme.lua
+++ lua/plugins/mfd_theme.lua
@@ -3,13 +3,18 @@ return {
   lazy = false,
   priority = 1000,
   config = function()
-    vim.cmd("colorscheme mfd-nerv")
-    vim.opt.guicursor = {
-      "n:block-CursorNormal",
-      "v:block-CursorVisual",
-      "i:block-CursorInsert",
-      "r-cr:block-CursorReplace",
-      "c:block-CursorCommand",
-    }
+    -- Let Ghostty's background show through the editor while keeping floating
+    -- windows opaque for readability.
+    -- for _, group in ipairs({ "Normal", "NormalNC", "SignColumn", "FoldColumn", "EndOfBuffer", "LineNr" }) do
+    --   vim.api.nvim_set_hl(0, group, { bg = "NONE" })
+    -- end
+
+    -- vim.opt.guicursor = {
+    --   "n:block-CursorNormal",
+    --   "v:block-CursorVisual",
+    --   "i:block-CursorInsert",
+    --   "r-cr:block-CursorReplace",
+    --   "c:block-CursorCommand",
+    -- }
   end,
 }
