2e895e9ad0ffa82f7b52c64dfb09ce68730703e0
Nick Marasciuolo <nickmdrummer@gmail.com>
2026-09-24 14:43:22 -0300

    (smear-cursor): new fun cursor animation

---
+++ lua/plugins/smear-cursor.lua
@@ -0,0 +1,11 @@
+return {
+  "sphamba/smear-cursor.nvim",
+
+  opts = {
+    cursor_color = "#ff4e40",
+    smear_between_buffers = true,
+    smear_between_neighbor_lines = true,
+    scroll_buffer_space = true,
+    smear_insert_mode = true,
+  },
+}
